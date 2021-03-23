35b98a1bd5412326916659c31439aa248c12ba73
Burak Kiper <30374154+delphi911@users.noreply.github.com>
2021-03-23 10:49:12 +0300

    Create controller.rb

---
+++ controller.rb
@@ -0,0 +1,22 @@
+class Controller
+        def play
+      puts "I have a secret code. Human Player please try to find my secret code in at most 12 turns."
+    end
+  
+    def ai
+      puts "Place make a secret code. Computer tries to find your secret code"
+    end
+  
+    def quit
+      puts "Goodbye."
+    end
+
+    def suggest
+      puts "My suggestion for secret code"
+    end
+
+    def startinggame
+        puts "Shall we play a game of mastermind?"
+        puts "Lets Start!."
+    end
+end #class Controller
